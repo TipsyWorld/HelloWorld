3cae61d18afc9c4bd1786875249a6bbc558b5f24
atlas <djlim@rsquare.co.kr>
2021-10-21 16:23:49 +0900

    [init] project

---
+++ MetroBot/MetroBot/MetroBotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetroBotApp.swift
+//  MetroBot
+//
+//  Created by Rsquare on 2021/10/21.
+//
+
+import SwiftUI
+
+@main
+struct MetroBotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
